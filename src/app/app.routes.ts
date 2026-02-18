8e7daf1a0500af87bce2c1b0f57aa74f83fd8319
Edu <serranotrenado.e@gmail.com>
2026-02-18 23:15:22 +0100

    created toutes and my profile view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './vistas/home/home';
+import { Perfil } from './vistas/perfil/perfil';
 
-export const routes: Routes = [];
-
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'perfil', component: Perfil },
+];
